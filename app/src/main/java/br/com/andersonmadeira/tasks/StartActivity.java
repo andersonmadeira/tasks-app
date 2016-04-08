b010eb6c34beda84c4b7d94585a5ed794c2109e0
Anderson Madeira <andersonmadeiracs@gmail.com>
2016-04-08 20:54:22 -0300

    Project Created.

---
+++ app/src/main/java/br/com/andersonmadeira/tasks/StartActivity.java
@@ -0,0 +1,17 @@
+package br.com.andersonmadeira.tasks; 
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle; 
+
+
+public class StartActivity extends AppCompatActivity { 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        getSupportActionBar().setElevation(0);
+    } 
+
+} 
